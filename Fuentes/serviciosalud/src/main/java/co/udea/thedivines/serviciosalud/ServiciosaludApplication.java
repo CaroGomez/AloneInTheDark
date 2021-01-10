053f1fc0786cb391c504683d9f90a7c498ad35fa
Carolina Díaz Gómez <astridc.diaz@udea.edu.co>
2021-01-09 20:10:38 -0500

    Creación proyecto
    Se crea el proyecto back y front de la implementación del servicio de salud, con java, maven, springboot y angular

---
+++ Fuentes/serviciosalud/src/main/java/co/udea/thedivines/serviciosalud/ServiciosaludApplication.java
@@ -0,0 +1,13 @@
+package co.udea.thedivines.serviciosalud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiciosaludApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiciosaludApplication.class, args);
+	}
+
+}
---
+++ Fuentes/serviciosalud/src/test/java/co/udea/thedivines/serviciosalud/ServiciosaludApplicationTests.java
@@ -0,0 +1,13 @@
+package co.udea.thedivines.serviciosalud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiciosaludApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
