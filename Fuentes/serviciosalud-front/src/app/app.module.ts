891a2d21c1a15d7ba491215414dfd598baf44d8e
Carolina Díaz Gómez <astridc.diaz@udea.edu.co>
2021-01-28 23:20:25 -0500

    vista inicial

--- Fuentes/serviciosalud-front/src/app/app.module.ts
+++ Fuentes/serviciosalud-front/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { HeaderComponent } from './header/header.component';
 import { CrearPacienteComponent } from './crear-paciente/crear-paciente.component';
 import { RouterModule, Router, Routes} from '@angular/router';
 import { MainComponent } from './main/main.component';
+import { CrearCitaComponent } from './crear-cita/crear-cita.component';
 
 const routes: Routes = [
   {path: '', component: MainComponent},
-  {path: 'crear-paciente', component: CrearPacienteComponent}
+  {path: 'crear-paciente', component: CrearPacienteComponent},
+  {path: 'crear-cita', component: CrearCitaComponent}
 
   ];
 
@@ -18,7 +20,8 @@ const routes: Routes = [
     AppComponent,
     HeaderComponent,
     CrearPacienteComponent,
-    MainComponent
+    MainComponent,
+    CrearCitaComponent
   ],
   imports: [
     BrowserModule,
